bfaa426652f473795f9eea18192aea479e693e78
aozp73 <aozp73@naver.com>
2024-01-24 21:14:00 +0900

    회원가입 - email / password, passwordConfirmation / username 컴포넌트화

---
+++ src/components/user/joinform/EmailInput.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const EmailInput = ({ user, setUser, setValidCheck, validCheck }) => {
+    const [emailCheckMessage, setEmailCheckMessage] = useState('');
+
+    useEffect(() => {
+        checkEmail();
+    }, [user.email]);
+
+    const checkEmail = async () => {
+        const email = user.email;
+        if (email === '') {
+            setEmailCheckMessage('');
+            setValidCheck({
+                ...validCheck,
+                email: false,
+            });
+            return;
+        }
+
+        axios.get(`http://localhost:8080/api/emailCheck?email=${email}`)
+            .then((res) => {
+                if (res.data.data) {
+                    setEmailCheckMessage('사용 가능한 이메일입니다.');
+                    setValidCheck({
+                        ...validCheck,
+                        email: true,
+                    });
+                } else {
+                    setEmailCheckMessage('이미 사용 중인 이메일입니다.');
+                    setValidCheck({
+                        ...validCheck,
+                        email: false,
+                    });
+                }})
+            .catch(error => console.error('이메일 중복 확인에 실패했습니다.', error))
+    };
+
+    const changeEmail = (e) => {
+        setUser({ ...user, email: e.target.value });
+    };
+
+    const emailCheckStyle = {
+        color: emailCheckMessage === '사용 가능한 이메일입니다.' ? 'green' : 'orangered',
+    };
+
+    return (
+        <div className="mb-3">
+            <input type="email" name="email" value={user.email} onChange={changeEmail}
+                   className="form-control" placeholder="이메일" required />
+            <div id="emailError" className="mt-2 mb-3 ms-2" style={emailCheckStyle}>
+                {emailCheckMessage}
+            </div>
+        </div>
+    );
+};
+
+export default EmailInput;
--- src/pages/user/JoinForm.js
+++ src/pages/user/JoinForm.js
@@ -1,120 +1,23 @@
-import React, {useEffect, useMemo, useState} from 'react';
-import axios from "axios";
+import React, {useState} from 'react';
+import EmailInput from "../../components/user/joinform/EmailInput";
+import PasswordInput from "../../components/user/joinform/PasswordInput";
+import UsernameInput from "../../components/user/joinform/UsernameInput";
 
 const JoinForm = () => {
-    // 입력 값
+
     const [user, setUser] = useState({
         email: '',
         password: '',
         passwordConfirmation: '',
         username: ''
     });
-    // 전송 전, 유효성 체크
     const [validCheck, setValidCheck] = useState({
         password: false,
         email: false,
+        username: false
     });
-    // 유효성 체크 안내
-    const validRegexs = {
-        password: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,12}$/, // 6-12글자 + 숫자-영문자 모두 포함
-        username: /^.{0,6}$/ // 최대 6글자
-    }
-    const [passwordCheckMessage, setPasswordCheckMessage] = useState('');
-    const [emailCheckMessage, setEmailCheckMessage] = useState('');
-    const [userCheckMessage, setUserCheckMessage] = useState('');
 
-    // 입력 값 랜더링
-    const changeValue = (e) => {
-        setUser({
-            ...user,
-            [e.target.name]: e.target.value,
-        });
-    };
-
-    // 유효성 체크 - 패스워드
-    const checkPassword = () => {
-        // 둘 다 비었을 경우
-        if (user.password === '' || user.passwordConfirmation === '') {
-            setPasswordCheckMessage('');
-            setValidCheck({
-                ...validCheck,
-                password: false,
-            });
-            return;
-        }
-
-        // 패스워드 일치
-        if (user.password === user.passwordConfirmation) {
-
-            // 패스워드 일치 -> 정규표현식
-            if (!validRegexs.password.test(user.password)) {
-                setPasswordCheckMessage('영어, 숫자를 조합해주세요. (6-12자)');
-                setValidCheck({
-                    ...validCheck,
-                    password: false,
-                });
-                return;
-            }
-
-            setPasswordCheckMessage('비밀번호 일치');
-            setValidCheck({
-                ...validCheck,
-                password: true,
-            });
-
-        // 패스워드 불일치
-        } else {
-            setPasswordCheckMessage('비밀번호가 일치하지 않습니다.');
-            setValidCheck({
-                ...validCheck,
-                password: false,
-            });
-        }
-    };
-
-    const passwordCheckStyle = {
-        color: passwordCheckMessage === '비밀번호 일치' ? 'green' : 'orangered',
-    };
-
-    useMemo(() => {
-        checkPassword();
-    }, [user.password, user.passwordConfirmation]);
-
-
-    const checkEmail = async () => {
-        const email = user.email;
-
-        if (email === '') {
-            setEmailCheckMessage('');
-            return;
-        }
-        axios.get(`http://localhost:8080/api/emailCheck?email=${email}`)
-            .then((res) => {
-                if (res.data.data) {
-                    setEmailCheckMessage('사용 가능한 이메일입니다.');
-                    setValidCheck({
-                        ...validCheck,
-                        email: true,
-                    });
-                } else {
-                    setEmailCheckMessage('이미 사용 중인 이메일입니다.');
-                    setValidCheck({
-                        ...validCheck,
-                        email: false,
-                    });
-                }})
-            .catch(error => console.error('이메일 중복 확인에 실패했습니다.', error))
-    };
-
-    const emailCheckStyle = {
-        color: emailCheckMessage === '사용 가능한 이메일입니다.' ? 'green' : 'orangered',
-    };
-
-    useMemo(() => {
-        checkEmail();
-    }, [user.email]);
-
-    // Server 전송 (submitRequirements로 확인 후, 통신 진행)
+    // Server 전송 (submitRequirements로 먼저 확인 -> 통신)
     const handleSave = (e) => {
         e.preventDefault();
         if (submitRequirements) {
@@ -129,47 +32,23 @@ const JoinForm = () => {
         user.passwordConfirmation &&
         user.username &&
         validCheck.password &&
-        validCheck.email;
+        validCheck.email &&
+        validCheck.username;
 
     return (
-        <div style={{ marginTop: '70px', marginBottom: '70px' }}>
+        <div style={{ marginTop: '70px', marginBottom: '130px' }}>
             <div className="custom-join-container mb-5 mt-5">
                 <div className="text-center" style={{ marginBottom: '35px' }}>
                     <h2>회원가입</h2>
                 </div>
                 <span></span>
                 <form onSubmit={handleSave}>
-                    <div className="mb-3">
-                        <input type="email" name="email" value={user.email} onChange={changeValue}
-                               className="form-control" placeholder="이메일" required />
-                        <div id="emailError" className="mt-2 mb-3 ms-2" style={emailCheckStyle}>
-                            {emailCheckMessage}
-                        </div>
-                    </div>
-                    <div className="mb-3">
-                        <input type="password" name="password" value={user.password} onChange={changeValue}
-                               className="form-control" placeholder="비밀번호" required />
-                        <div id="passwordError" className="mt-2 mb-3 ms-2" style={{ color: 'orangered' }}>
-                        </div>
-                    </div>
-                    <div>
-                        <input type="password" name="passwordConfirmation" value={user.passwordConfirmation} onChange={changeValue}
-                               className="form-control" placeholder="비밀번호 확인" required />
-                        <div id="passwordConfirmationError" className="mt-2 mb-3 ms-2" style={passwordCheckStyle}>
-                            {passwordCheckMessage}
-                        </div>
-                    </div>
-                    <div className="mb-5">
-                        <input type="text" name="username" value={user.username} onChange={changeValue}
-                               className="form-control" placeholder="아이디" required />
-                        <div className="mt-2 mb-3 ms-2" style={{ color: 'orangered' }}>
-                        </div>
-                    </div>
-                    <div className="mb-2">
-                        <button className="btn btn-primary custom-join-btn">
-                            등록하기
-                        </button>
-                    </div>
+                    <EmailInput user={user} setUser={setUser} validCheck={validCheck} setValidCheck={setValidCheck} />
+                    <PasswordInput user={user} setUser={setUser} validCheck={validCheck} setValidCheck={setValidCheck}/>
+                    <UsernameInput user={user} setUser={setUser} validCheck={validCheck} setValidCheck={setValidCheck}/>
+                    <button className="btn btn-primary custom-join-btn">
+                        등록하기
+                    </button>
                 </form>
             </div>
         </div>
