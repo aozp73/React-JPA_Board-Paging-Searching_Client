a77a25ca977c6130648450eef40d686d86afeb82
aozp73 <aozp73@naver.com>
2024-01-24 21:30:11 +0900

    회원가입 - axios 통신

--- src/pages/user/JoinForm.js
+++ src/pages/user/JoinForm.js
@@ -2,9 +2,11 @@ import React, {useState} from 'react';
 import EmailInput from "../../components/user/joinform/EmailInput";
 import PasswordInput from "../../components/user/joinform/PasswordInput";
 import UsernameInput from "../../components/user/joinform/UsernameInput";
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
 const JoinForm = () => {
-
+    const navigate = useNavigate();
     const [user, setUser] = useState({
         email: '',
         password: '',
@@ -20,8 +22,31 @@ const JoinForm = () => {
     // Server 전송 (submitRequirements로 먼저 확인 -> 통신)
     const handleSave = (e) => {
         e.preventDefault();
+
         if (submitRequirements) {
-            console.log("모든 입력 값 Valid 통과")
+
+            axios.post(`http://localhost:8080/api/join`, user)
+                .then((res) => {
+                    alert("회원가입 완료");
+                    navigate('/loginForm');
+                    console.log('회원가입 성공:', res.data);
+                })
+                .catch(error => {
+                    if (error.response) {
+                        if (error.response.status === 400) {
+                            console.error('400 Error:', error.response.data);
+                            alert("입력 값을 확인해주세요.");
+                        } else if (error.response.status === 500) {
+                            console.error('500 Error:', error.response.data);
+                            alert("서버 오류가 발생했습니다. 잠시 후 다시 시도해주세요.");
+                        }
+                    } else {
+                        // 서버에서 응답이 없거나 요청을 전송할 수 없는 경우
+                        console.error('Error:', error.message);
+                        alert("네트워크 오류가 발생했습니다.");
+                    }
+                });
+
         } else {
             alert("입력 값을 확인해주세요.")
         }
