cad35f6152ca97526248a1e5a598d62fff9a6145
aozp73 <aozp73@naver.com>
2024-01-25 16:36:20 +0900

    로그인 - axios 통신 및 응답 데이터 확인

--- src/pages/user/LoginForm.js
+++ src/pages/user/LoginForm.js
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from "react-router-dom";
+import axios from "axios";
 
 const LoginForm = () => {
+
+    const [user, setUser] = useState({
+        email: '',
+        password: '',
+    });
+
+    const changeValue = (e) => {
+        setUser({ ...user, [e.target.name]: e.target.value });
+    };
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+        axios.post(`http://localhost:8080/api/login`, user)
+            .then((res) => {
+                console.log(res);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
     return (
         <div style={{ marginTop: '70px', marginBottom: '100px' }}>
             <div className="custom-login-container mb-5 mt-5">
-                <form action="/login" method="POST">
-                    {/*<div className="alert alert-danger" style={{ display: 'none' }}>*/}
-                    {/*    올바르지 않은 정보입니다.*/}
-                    {/*</div>*/}
+                <form onSubmit={handleLogin}>
+                    <div className="alert alert-danger" style={{ display: 'none' }}>
+                        올바르지 않은 정보입니다.
+                    </div>
                     <div className="mb-3">
-                        <input type="email" name="username"
+                        <input type="email" name="email" value={user.email} onChange={changeValue}
                                className="form-control" placeholder="이메일" required />
                     </div>
                     <div className="mb-4">
-                        <input type="password" name="password" className="form-control"
-                               placeholder="비밀번호" required />
+                        <input type="password" name="password" value={user.password} onChange={changeValue}
+                               className="form-control" placeholder="비밀번호" required />
                     </div>
                     <div>
                         <div className="mb-2">
